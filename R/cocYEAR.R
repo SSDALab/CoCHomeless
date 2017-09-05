a35d18e86b0f44d28c8a9a26d7fd5dc899e60e9d
zalmquist <zalmquist@gmail.com>
2017-09-05 15:58:38 -0700

    package 0.1.0" git push
    ls

--- R/cocYEAR.R
+++ R/cocYEAR.R
@@ -1,11 +1,112 @@
-#' 2007 Continuum of Care (CoC) GIS shapefiles
+#' 2005 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2005"
+
+#' 2006 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2006"
+
+#' 2007 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
 #'
 #' These GIS shapefiles contain the geographic boundaries and funding status information
 #' for HUD’s Continuum of Care (CoC) areas by Year.
 #'
 #' @source https://www.hudexchange.info/programs/coc/gis-tools/
 #' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
-"coc_2007"
+"CoC_2007"
+
+#' 2008 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2008"
+
+#' 2009 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2009"
+
+#' 2010 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2010"
+
+#' 2011 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2011"
+
+#' 2012 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2012"
+
+#' 2013 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2013"
+
+#' 2014 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2014"
+
+#' 2015 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2015"
+
+#' 2016 Continuum of Care (CoC) GIS shapefiles and Homeless Counts
+#'
+#' These GIS shapefiles contain the geographic boundaries and funding status information
+#' for HUD’s Continuum of Care (CoC) areas by Year.
+#'
+#' @source https://www.hudexchange.info/programs/coc/gis-tools/
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_2016"
 
 #' County level homeless counts and Shapefiles
 #'
@@ -13,25 +114,161 @@
 #' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
 #' and imputed to align with US County shape files.
 #'
-#' @format A data frame with 3143 rows and 20 variables:
+#' @format A data frame with 3143 rows:
 #' \describe{
-#'   \item{}{}
 #' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
 #' @source http://users.stat.umn.edu/~almquist/software.html
 #' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
-"USCounty2010"
+"USCountyTotalHomeless"
 
-#' County level homeless counts
+#' County level homeless counts, 2007
 #'
 #' Completed county-level homeless counts (with missing values imputed) and selected
 #' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
 #' and imputed to align with US County shape files.
 #'
-#' @format A data frame with 3143 rows and 20 variables:
+#' @format A data frame with 3143 rows:
 #' \describe{
-#'   \item{}{}
 #' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
 #' @source http://users.stat.umn.edu/~almquist/software.html
 #' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
-"CountyHomeless2007"
+"CoC_DF_2007"
 
+#' County level homeless counts, 2008
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2008"
+
+#' County level homeless counts, 2009
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2009"
+
+#' County level homeless counts, 2010
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2010"
+
+#' County level homeless counts, 2011
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2011"
+
+#' County level homeless counts, 2012
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2012"
+
+#' County level homeless counts, 2013
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2013"
+
+#' County level homeless counts, 2014
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2014"
+
+#' County level homeless counts, 2015
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2015"
+
+#' County level homeless counts, 2016
+#'
+#' Completed county-level homeless counts (with missing values imputed) and selected
+#' demographic variables SF 1 Summary file. HUD provided CoC counts disaggregated
+#' and imputed to align with US County shape files.
+#'
+#' @format A data frame with 3143 rows:
+#' \describe{
+#' }
+#' @source Almquist, Helwig and You (2017). Working Paper.
+#' @source http://users.stat.umn.edu/~almquist/software.html
+#' @source https://www.hudexchange.info/resource/3031/pit-and-hic-data-since-2007/
+"CoC_DF_2016"
+
+
+#' US State polygon files
+#'
+#' Completed US State Polygon Files
+#'
+#' @format Spatial Polygon DataFrame:
+#' \describe{
+#' }
+#' @source US Census Bureau
+"CoC_DF_2016"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# COC_Homeless
+# CoC_Homeless
 Continuum of Care Data for the United States.
+
+- CoC Shape files and counts
+- Disaggregated and imputed matching for CoC data to County level data
+- See Working Paper (in review)
